5498ed7d2628c978838cc5ef144548b1f03104d6
Westporch <westporch@gmail.com>
2015-10-13 17:47:11 +0900

    Initial

---
+++ hello.c
@@ -0,0 +1,19 @@
+#include <linux/init.h>
+#include <linux/module.h>
+
+MODULE_LICENSE("Dual BSD/GPL");
+MODULE_AUTHOR("HyunGwan Seo <westporch@gmail.com>");
+MODULE_DESCRIPTION("My first kernel module");
+
+static int hello_init(void)
+{
+	printk(KERN_ALERT "Hello, World!!\n");
+}
+
+static void hello_exit(void)
+{
+	printk(KERN_ALERT "Goodbye :-)\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
